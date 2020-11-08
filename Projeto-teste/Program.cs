1b566890b7083b8b6a43c874c87fe2863de09772
Ricardo Lazaro <ricardoalazaro@hotmail.com>
2020-11-08 10:17:05 -0300

    Acerscentada uma mensagem de BOM DIA!

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -7,6 +7,9 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
+
+            Console.WriteLine();
         }
     }
 }
